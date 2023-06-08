8af258790d02feba9e27b8adce9130ed605dd4d4
hosose <hosose22@gmail.com>
2023-06-08 17:29:57 +0900

    Add : PhoneNumberVO

---
+++ webstudy88-pks-travel/src/main/java/org/kosta/myproject/model/PhoneNumberVO.java
@@ -0,0 +1,23 @@
+package org.kosta.myproject.model;
+
+public class PhoneNumberVO {
+	private int phoneNumber;
+
+	public PhoneNumberVO() {
+		super();
+	}
+
+	public PhoneNumberVO(int phoneNumber) {
+		super();
+		this.phoneNumber = phoneNumber;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+}
